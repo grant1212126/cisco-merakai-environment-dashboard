49471adcdc0f08fa9fa0f94fc5418655ba3d4921
Mate Kukri <kukri.mate@gmail.com>
2021-11-09 11:44:20 +0100

    Add API demonstration with 3 views
    Index: table of organizations
    From the index page there are per org views for:
    * Each organization's networks
    * And each organization's list of devices

--- Dashboard/settings.py
+++ Dashboard/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Dashboard.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / "templates" ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- DashboardApp/views.py
+++ DashboardApp/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import meraki
 
+# Cisco API handle
+dashboard = meraki.DashboardAPI()
+
+# List of organizations
 def index(request):
-    return HttpResponse("Hello, World!")
+    return render(request, "index.html", {
+        "orgs": dashboard.organizations.getOrganizations()
+    })
+
+# Networks per org
+def networks(request, org_id):
+    return render(request, "networks.html", {
+        "org": dashboard.organizations.getOrganization(org_id),
+        "nets": dashboard.organizations.getOrganizationNetworks(org_id),
+        })
+
+# Devices per org
+def devices(request, org_id):
+    return render(request, "devices.html", {
+        "org": dashboard.organizations.getOrganization(org_id),
+        "devs": dashboard.organizations.getOrganizationDevices(org_id),
+        })
