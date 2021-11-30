f4739a162568a27769cfe0e1b1583f31f616954c
Grant Simpson <2466525S@student.gla.ac.uk>
2021-11-30 16:20:25 +0000

    Show DataPoints

--- DashboardApp/urls.py
+++ DashboardApp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, networks, devices
+from .views import index, networks, devices, data
 
 urlpatterns = [
     path("", index, name="index"),
     path("<int:org_id>/networks", networks, name="networks"),
     path("<int:org_id>/devices", devices, name="devices"),
+    path("<int:org_id>/data", data, name="data"),
 ]
--- DashboardApp/views.py
+++ DashboardApp/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from DashboardApp.models import DataPoint
 import meraki
 
 # Cisco API handle
-dashboard = meraki.DashboardAPI(suppress_logging=True)
+dashboard = meraki.DashboardAPI("10a7351614bbbc50e3e2f91a53e15cb58636a6a6", suppress_logging=True)
 
 # List of organizations
 def index(request):
@@ -24,3 +25,11 @@ def devices(request, org_id):
         "org": dashboard.organizations.getOrganization(org_id),
         "devs": dashboard.organizations.getOrganizationDevices(org_id),
         })
+
+def data(request, org_id):
+    
+    return render(request, "data.html", {
+        "org": dashboard.organizations.getOrganization(org_id),
+        "devs": dashboard.organizations.getOrganizationDevices(org_id),
+        "data": DataPoint.objects.all(),
+    })
