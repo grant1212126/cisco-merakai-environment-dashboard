b0351ac34a8cf0f95963fdb0c1df5009e6a25d5d
Richard Bozko <2472271b@student.gla.ac.uk>
2021-11-30 19:33:05 +0000

    bare-bones data-vis

--- DashboardApp/urls.py
+++ DashboardApp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import index, networks, devices
+from .views import index, networks, devices, visualize_data
 
 urlpatterns = [
     path("", index, name="index"),
     path("<int:org_id>/networks", networks, name="networks"),
     path("<int:org_id>/devices", devices, name="devices"),
+    path("charts/", visualize_data, name = "charts"),
+
+
+
 ]
--- DashboardApp/views.py
+++ DashboardApp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import DataPoint
 import meraki
+import datetime
+
+
 
 # Cisco API handle
 dashboard = meraki.DashboardAPI(suppress_logging=True)
@@ -24,3 +28,38 @@ def devices(request, org_id):
         "org": dashboard.organizations.getOrganization(org_id),
         "devs": dashboard.organizations.getOrganizationDevices(org_id),
         })
+
+
+def visualize_data(request):
+    data = {
+        "humidity_data": DataPoint.objects.filter(
+            sensor = "HD").values_list("value","tstamp"),
+        "temperature_data": DataPoint.objects.filter(
+            sensor = "TM").values_list("value","tstamp"),
+        "occupancy_data": DataPoint.objects.filter(
+            sensor = "OC").values_list("value","tstamp"),
+    }
+
+    if request.is_ajax():
+        print("was AJAX")
+        chart_data = {
+
+                "humidity_data": [
+                    [data[0] for data in data["humidity_data"]],
+                    [data[1].strftime("%m-%d-%Y-%H:%M:%S") for data in data["humidity_data"]]
+                ],
+                "temperature_data": [
+                    [data[0] for data in data["temperature_data"]],
+                    [data[1].strftime("%m-%d-%Y-%H:%M:%S") for data in data["temperature_data"]]
+                ],
+                "occupancy_data": [
+                    [data[0] for data in data["occupancy_data"]],
+                    [data[1].strftime("%m-%d-%Y-%H:%M:%S") for data in data["occupancy_data"]]
+                ],
+
+        }
+
+        return JsonResponse(data = chart_data, safe = False)
+
+    return render(request, "charts.html")
+
