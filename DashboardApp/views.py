1d020aadc7c4fdf98a7a587a30e7fb16bdcee1f4
Mate Kukri <km@mkukri.xyz>
2022-01-10 11:45:01 +0000

    Add initial support for the Meraki sensor API.
    In particular this commit makes the following changes:
    * Make the charts the homepage, some slight improvement to the templates
      along the way
    * Add a new view called "add_device" for searching for and adding Meraki
      sensors without using the database adminisration interface
    * Finally implement Meraki sensor support in the data gathering daemon.
      This required replacing the Meraki library with some simple custom API
      wrappers, as the MT10 endpoints are still beta and are not supported
      by said library.
    
    The following issues are resolved by this work or previous work by
    others: #15, #14, #13, #12, #11.

--- DashboardApp/views.py
+++ DashboardApp/views.py
@@ -1,45 +1,76 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from .models import DataPoint
-from DashboardApp.models import DataPoint
-import meraki
+from DashboardApp.models import DataPoint, MerakiSensor, MerakiDataSource
+from lib import meraki
 import datetime
+import json
 
+# Sets up add_device.html and adds device when posted
+def add_device(request):
+    class Device:
+        def __init__(self, org_id, serial, description, supported_sensors):
+            self.org_id = org_id
+            self.serial = serial
+            self.description = description
+            self.supported_sensors = supported_sensors
 
+    if request.method == "POST":
+        org_id = request.POST.get("org_id")
+        serial = request.POST.get("serial")
+        description = request.POST.get("description")
 
-# Cisco API handle
-dashboard = meraki.DashboardAPI(suppress_logging=True)
+        def add_sensor(sensor):
+            ds = MerakiDataSource(org_id=org_id,
+                    serial=serial,
+                    description=description,
+                    interval=interval,
+                    sensor=sensor)
+            ds.save()
 
-# List of organizations
-def index(request):
-    return render(request, "index.html", {
-        "orgs": dashboard.organizations.getOrganizations()
-    })
+        interval = 10 # FIXME: stop hard-coding this
+        if request.POST.get("sensor_oc") == "on":
+            add_sensor(MerakiSensor.OCCUPANCY)
+        if request.POST.get("sensor_tm") == "on":
+            add_sensor(MerakiSensor.TEMPERATURE)
+        if request.POST.get("sensor_hd") == "on":
+            add_sensor(MerakiSensor.HUMIDITY)
 
-# Networks per org
-def networks(request, org_id):
-    return render(request, "networks.html", {
-        "org": dashboard.organizations.getOrganization(org_id),
-        "nets": dashboard.organizations.getOrganizationNetworks(org_id),
-        })
+    elif request.is_ajax():
+        org_id = request.GET.get("org_id")
+        if not isinstance(org_id, str):
+            raise ValueError()
 
-# Devices per org
-def devices(request, org_id):
-    return render(request, "devices.html", {
-        "org": dashboard.organizations.getOrganization(org_id),
-        "devs": dashboard.organizations.getOrganizationDevices(org_id),
-        })
+        devices = []
+        for meraki_dev in meraki.get_devices(org_id):
+            print(meraki_dev)
+            supported_sensors = []
 
+            if meraki_dev["productType"] == "camera":
+                supported_sensors.append(MerakiSensor.OCCUPANCY)
+            elif meraki_dev["productType"] == "sensor":
+                # TODO: we might want to check if a "sensor" device truly
+                # supports these metrics, for now I am just assuming its
+                # always gonna be an MT10
+                supported_sensors.append(MerakiSensor.HUMIDITY)
+                supported_sensors.append(MerakiSensor.TEMPERATURE)
 
-# Feeds data to charts.html
+            device = Device(org_id, meraki_dev["serial"],
+                            meraki_dev["name"], supported_sensors)
+            devices.append(device.__dict__)
+
+        return JsonResponse(data=devices, safe=False)
+
+    return render(request, "add_device.html")
+
+# Feeds data to index.html
 def visualize_data(request):
     data = {
         "humidity_data": DataPoint.objects.filter(
-            sensor = "HD").values_list("value","tstamp"),
+            sensor = "HD").values_list("value", "tstamp"),
         "temperature_data": DataPoint.objects.filter(
-            sensor = "TM").values_list("value","tstamp"),
+            sensor = "TM").values_list("value", "tstamp"),
         "occupancy_data": DataPoint.objects.filter(
-            sensor = "OC").values_list("value","tstamp"),
+            sensor = "OC").values_list("value", "tstamp"),
     }
 
     # Dictionaries structured in a Chart JS compatible way
@@ -91,16 +122,6 @@ def visualize_data(request):
         }
 
         
-        return JsonResponse(data = chart_data , safe = False)
+        return JsonResponse(data=chart_data, safe=False)
 
-    return render(request, "charts.html")
-
-
-def data(request, org_id):
-    
-    return render(request, "data.html", {
-        "org": dashboard.organizations.getOrganization(org_id),
-        "devs": dashboard.organizations.getOrganizationDevices(org_id),
-        "data": DataPoint.objects.all(),
-    })
-
+    return render(request, "index.html")
