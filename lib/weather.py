85a996deecb8e1b170cc9c7b270b92b6f31a7341
Mate Kukri <km@mkukri.xyz>
2022-02-03 12:47:38 +0000

    Implement support for "Locations", add a new settings panel

--- lib/weather.py
+++ lib/weather.py
@@ -2,18 +2,25 @@
 # OpenWeatherMap API wrappers
 #
 
+import gzip
+import json
 import os
+import pickle
 import requests
 
-class OpenWeatherMapError(BaseException):
-    def __init__(self, status, message):
-        super().__init__(f"{status}: {message}")
+
+# Absolute path to city list file
+city_list = os.path.join(os.path.dirname(__file__), "city.list.pickled")
 
 # Base URL for the OpenWeatherMap API
 base_url = "https://api.openweathermap.org/data/2.5"
 
 # Our API key is always in this environment variable
 apikey = os.getenv("OPENWEATHERMAP_API_KEY")
+
+class OpenWeatherMapError(BaseException):
+    def __init__(self, status, message):
+        super().__init__(f"{status}: {message}")
 
 def read_city_weather(city):
     # API endpoint
@@ -38,3 +45,8 @@ def read_loc_weather(lat, lon):
         raise OpenWeatherMapError(resp.status_code, resp.content)
     # Parse response
     return resp.json()
+
+def search_city(name):
+    with open(city_list, "rb") as file:
+        lookup = pickle.load(file)
+    return lookup.get(name)
